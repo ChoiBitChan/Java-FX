e2cbf91975fc66a66501ad807644d3213c886de0
B_Chan <lucete1104@gmail.com>
2016-12-06 21:19:02 +0900

    12.6

--- src/com/home/study/fx/Window.java
+++ src/com/home/study/fx/Window.java
@@ -2,6 +2,7 @@ package com.home.study.fx;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,12 +16,17 @@ public class Window extends JFrame implements Runnable {
 	private int xCnt, yCnt; // 칸 개수
 	private int sxCnt, syCnt;
 	private int nxCnt, nyCnt;
-	
-	private int area; // 단위 ex:20
 
-	//private boolean[][] grid;
+	private int area; // 단위 ex:20
+	private int time;
+
+	// private boolean[][] grid;
 	private JPanel[][] background;
 	private JPanel[][] subground;
+	private ArrayList<Window_Item> itemList;
+
+	private Window_Item nextItem;
+	private Window_Item currentItem;
 
 	private Container fc;
 
@@ -28,8 +34,9 @@ public class Window extends JFrame implements Runnable {
 
 	private final Color bgColor = Color.black;
 	private final Color subColor = Color.white;
-	private final Color nextColor = Color.black;
-	
+	private final Color nextColor = Color.gray;
+
+	Thread t;
 
 	public Window(String str) {
 
@@ -37,17 +44,21 @@ public class Window extends JFrame implements Runnable {
 		this.xCnt = 14;
 		this.yCnt = 25;
 		this.sxCnt = 10;
-		this.nxCnt = 2;
-		this.nyCnt = 6;
+		this.nxCnt = 3;
+		this.nyCnt = 4;
+		
+		this.time = 500;
 
 		this.area = 20;
 
 		this.width = this.xCnt * this.area;
 		this.height = this.yCnt * this.area;
 
+		this.itemList = new ArrayList<Window_Item>();
+
 		this.background = new JPanel[this.xCnt][this.yCnt];
 		this.subground = new JPanel[this.sxCnt][this.yCnt];
-		//this.grid = new boolean[this.xCnt][this.yCnt]; // 블럭의 유무를 알기 위해서 생성
+		// this.grid = new boolean[this.xCnt][this.yCnt]; // 블럭의 유무를 알기 위해서 생성
 
 		this.fc = this.getContentPane();
 
@@ -58,23 +69,24 @@ public class Window extends JFrame implements Runnable {
 
 		this.setBounds(200, 200, this.width + (this.sxCnt * this.area) + 36, this.height + 45);
 
+		itemList.add(new Rect(this.area, this.center, this.xCnt));
+
 		this.sub = new JPanel();
 		this.next = new JPanel();
 		this.sub.setBounds(this.width + 10, 0, this.sxCnt * this.area, this.height);
 		this.sub.setBackground(this.subColor);
 		this.sub.setBorder(new SoftBevelBorder(BevelBorder.RAISED));
-		
-		
-		this.next.setBounds(this.nxCnt * this.area, this.nxCnt * this.area, this.nyCnt * this.area, this.nyCnt * this.area);
+
+		this.next.setBounds(this.nxCnt * this.area, 1 * this.area, this.nyCnt * this.area, this.nyCnt * this.area);
 		this.next.setBackground(this.nextColor);
 		this.next.setBorder(new SoftBevelBorder(BevelBorder.RAISED));
-		
+
 		this.center.add(this.sub);
 		this.sub.setLayout(null);
 		this.sub.add(this.next);
 
-		for (int i = 0; i < background.length; i++) {
-			for (int p = 0; p < background[i].length; p++) {
+		for (int i = 0; i < background.length; i++) { // 0~13 14회
+			for (int p = 0; p < background[i].length; p++) { // 0~24 25회
 				this.background[i][p] = new JPanel();
 				this.background[i][p].setBounds(i * this.area, p * this.area, this.area, this.area);
 				this.background[i][p].setBackground(this.bgColor);
@@ -82,7 +94,7 @@ public class Window extends JFrame implements Runnable {
 				this.center.add(background[i][p]);
 			}
 		}
-		
+
 		for (int i = 0; i < subground.length; i++) {
 			for (int p = 0; p < subground[i].length; p++) {
 				this.subground[i][p] = new JPanel();
@@ -92,14 +104,17 @@ public class Window extends JFrame implements Runnable {
 				this.sub.add(subground[i][p]);
 			}
 		}
-		
-		
+
+		this.currentItem = itemList.get(0);
+		this.currentItem.setColor(Color.YELLOW);
+		this.currentItem.setDefaultLocation();
+		setNextItem();
+
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    this.setVisible(true);
-		
-		
-		
-		
+		this.setVisible(true);
+
+		t = new Thread(this);
+		t.start();
 
 	}
 
@@ -107,6 +122,65 @@ public class Window extends JFrame implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
 
+		try {
+			while (true) {
+				Thread.sleep(this.time);
+
+				goDown();
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+	public boolean goDown() {
+		Block[] tempBlock = this.currentItem.getBlock();
+
+		for (int i = 0; i < tempBlock.length; i++) {
+			int x = tempBlock[i].getX();
+			int y = tempBlock[i].getY() + 1;
+			if (y >= this.yCnt) {
+
+				setCopyBlock();
+				setNewItem();
+				return false;
+			}
+		}
+
+		this.currentItem.moveDown();
+		return true;
+	}
+
+	public void setCopyBlock() {
+		Block[] tempBlock = this.currentItem.getBlock();
+		for (int i = 0; i < tempBlock.length; i++) { // i는 0부터 3까지
+			setBack(tempBlock[i].getX(), tempBlock[i].getY(), this.currentItem.getColor());
+		}
+		this.currentItem.setReadyLocation();
+	}
+
+	public void setBack(int x, int y, Color c) {
+		this.background[x][y].setBackground(c);
+		this.background[x][y].setBorder(new SoftBevelBorder(BevelBorder.RAISED));
+
+	}
+
+	public void setNewItem() {
+		this.currentItem = this.nextItem;
+		this.currentItem.setDefaultLocation();
+		setNextItem();
+	}
+
+	public void setNextItem() {
+		Window_Item temp;
+
+		temp = itemList.get(0);
+		this.nextItem = temp;
+		this.nextItem.setColor(Color.YELLOW);
+		this.nextItem.setNextLocation();
+		
 	}
 
 	public static void main(String[] args) {
--- src/com/home/study/fx/Window_Item.java
+++ src/com/home/study/fx/Window_Item.java
@@ -1,8 +1,195 @@
 package com.home.study.fx;
 
-class Block {}
+import java.awt.Color;
+import java.awt.Container;
 
-public class Window_Item {}
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.SoftBevelBorder;
 
 
+class Block {
 
+	private int x;
+	private int y;
+
+	// 생성자
+	public Block() {
+	}
+
+	public Block(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	// 해당 포인트만큼 감산
+	public void move(int xPlus, int yPlus) {
+		this.x += xPlus;
+		this.y += yPlus;
+	}
+
+	// X좌표 반환
+	public int getX() {
+		return this.x;
+	}
+
+	// Y좌표 반환
+	public int getY() {
+		return this.y;
+	}
+
+	// 자신 반환
+	public Block getBlock() {
+		return this;
+	}
+
+	// XY세팅
+	public void setXY(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
+
+public class Window_Item {
+
+	JPanel[] panel;
+	Block[] block; // 현재포인트 좌표
+	Block[][] block_info; // 각도별 포인트
+
+	Block currentXY;
+	int cnt; // 총판넬 개수
+	int angle; // 총각도 개수
+	int current_angle; // 현재 각도값
+	int xCnt; // 가로값
+
+	Color color = Color.YELLOW;
+	int area; // 넓이
+
+	public Window_Item(int area, int angle, int cnt, int xCnt) {
+		this.angle = angle; // 각도 개수
+		this.cnt = cnt; // cnt = 4
+
+		this.panel = new JPanel[cnt];
+		this.block = new Block[cnt];
+		this.block_info = new Block[angle][cnt];
+		this.area = area; // area = 20
+
+		this.currentXY = new Block(0, 0); // 현재 값
+		this.xCnt = xCnt; // xCnt = 14
+
+		for (int i = 0; i < cnt; i++) { // 패널 생성 cnt = 4;
+			this.panel[i] = new JPanel(); // 모든 블럭은 4개의 패널만 생성
+		}
+	}
+
+	public void setDefaultRandom() {
+		this.current_angle = (int) (Math.random() * this.angle);
+
+		this.block = this.block_info[this.current_angle];
+
+	}
+
+	public void setItem(Container c) {
+		for (int i = 0; i < panel.length; i++) {
+
+			panel[i].setBackground(this.color);
+			panel[i].setSize(area, area);
+			panel[i].setLocation(((block[i].getX()) * area) - 100, ((block[i].getY()) * area) - 100);
+
+			panel[i].setBorder(new SoftBevelBorder(BevelBorder.RAISED));
+
+			c.add(panel[i]);
+		}
+	}
+
+	public void setNextLocation() {
+		for (int i = 0; i < panel.length; i++) {
+			int x = block[i].getX() + (xCnt - 3);
+			int y = block[i].getY() + 1;
+			panel[i].setLocation(x * area, y * area);
+		}
+		this.currentXY.setXY((xCnt - 3), 1);
+	}
+
+	public void setDefaultLocation() {
+		for (int i = 0; i < panel.length; i++) {
+			int x = block[i].getX() + (int) (xCnt / 2 - 2);
+
+			int y = block[i].getY() + 2;
+
+			panel[i].setLocation(x * area, y * area);
+
+		}
+		this.currentXY.setXY((int) (xCnt / 2 - 2), 3);
+	}
+
+	public void setReadyLocation() {
+		for (int i = 0; i < panel.length; i++) {
+			panel[i].setLocation(((block[i].getX()) * area) - 100, ((block[i].getY()) * area) - 100);
+
+		}
+	}
+
+	public Block getCurrentXY() {
+		return this.currentXY;
+	}
+
+	public void setCurrentXY(int x, int y) {
+		this.currentXY.move(x, y);
+	}
+
+	public Block[] getBlock() {
+		Block[] temp = new Block[cnt];
+		for (int i = 0; i < block.length; i++) {
+			int x = block[i].getX() + this.currentXY.getX();
+			int y = block[i].getY() + this.currentXY.getY();
+			temp[i] = new Block(x, y);
+		}
+		return temp;
+	}
+
+	public void setColor(Color c) {
+
+		this.color = c;
+		for (int i = 0; i < panel.length; i++) {
+			panel[i].setBackground(this.color);
+		}
+	}
+
+	public void setMove() {
+		for (int i = 0; i < panel.length; i++) {
+
+			int x = this.block[i].getX() + this.currentXY.getX();
+			int y = this.block[i].getY() + this.currentXY.getY();
+
+			panel[i].setLocation(x * area, y * area);
+		}
+	}
+
+	public void moveDown() {
+		this.currentXY.move(0, 1);
+		this.setMove();
+	}
+
+	public Color getColor() {
+		return this.color;
+	}
+
+}
+
+class Rect extends Window_Item {
+
+	public Rect(int area, Container con, int xCnt) {
+		super(area, 1, 4, xCnt);
+
+		block_info[0][0] = new Block(0, 0);
+		block_info[0][1] = new Block(0, 1);
+		block_info[0][2] = new Block(1, 0);
+		block_info[0][3] = new Block(1, 1);
+
+		this.setDefaultRandom();
+		this.setItem(con);
+
+	}
+}
