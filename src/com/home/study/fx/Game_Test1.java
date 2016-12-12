b9f3989562888997b9e6f3214be649f1967b30b1
최빛찬 <@PC>
2016-12-13 00:16:06 +0900

    12.16

--- src/com/home/study/fx/Game_Test1.java
+++ src/com/home/study/fx/Game_Test1.java
@@ -10,31 +10,35 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Game_Test1 extends Application {
-
+	
+	GridPane grid = new GridPane();
+	StackPane pane = new StackPane();
+	Scene scene = new Scene(pane, 500, 500);
+	
+	Rectangle[][] block;
+	GridPane background;
 	
 	@Override
 	public void start(Stage stage) throws Exception {
 		// TODO Auto-generated method stub
 		
-		Rectangle block;
-		
 		try {
+			Rectangle rect;
+			block = new Rectangle[10][10];
 			for(int i = 0; i < 10; i++){
 				for(int j = 0; j < 10; j++){
-					block = new Rectangle(5,5);
-					block.setFill(Color.GRAY);
+					rect = new Rectangle(5, 5);
+					rect.setFill(Color.GRAY);
+					block[i][j] = rect;
+					background.add(rect, j, i);
 					
 					
-					grid.add(block, i, j);
+					//grid.add(block, i, j);
 					
 				}
 			}
 					
-				
-			
-		
-			
-			pane.getChildren().add(grid);
+			pane.getChildren().add(background);
 			
 			stage.setScene(scene);
 			stage.show();
@@ -45,11 +49,6 @@ public class Game_Test1 extends Application {
 		}
 
 	}
-	
-	GridPane grid = new GridPane();
-	StackPane pane = new StackPane();
-	Scene scene = new Scene(pane, 500, 500);
-	
 	
 	public static void main(String[] args) {
 		launch(args);
